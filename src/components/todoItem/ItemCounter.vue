b963165001154f17329398ef8d2ca35bc3cefa16
Kegen Guyll <guyllkegen@gmail.com>
2021-03-24 19:34:59 -0500

    created progress counter

---
+++ src/components/todoItem/ItemCounter.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <v-chip-group>
+      <v-chip class="text-white" label color="green">
+        {{ `Tasks Completed ${completed}` }}
+      </v-chip>
+      <v-chip class="text-white" label color="orange">
+        {{ `Tasks In Progress ${inProgress}` }}
+      </v-chip>
+    </v-chip-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ItemCounter',
+  props: ['tasks'],
+  data() {
+    return {
+      completed: 0,
+      inProgress: 0,
+    };
+  },
+  watch: {
+    tasks: {
+      handler: function(newVal) {
+        let tempCompleted = 0;
+        let tempInProgress = 0;
+        newVal.forEach((task) => {
+          if (task.complete) {
+            tempCompleted = tempCompleted + 1;
+          } else {
+            tempInProgress = tempInProgress + 1;
+          }
+
+          this.completed = tempCompleted;
+          this.inProgress = tempInProgress;
+        });
+      },
+      deep: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.text-white {
+  color: white !important;
+}
+</style>
