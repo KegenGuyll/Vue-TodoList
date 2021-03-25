66c1b8ec1fc4cc0b18404bb7b014db176ed68ec1
Kegen Guyll <guyllkegen@gmail.com>
2021-03-25 15:43:23 -0500

    removed all un-needed code

--- src/components/todoItem/ItemCounter.vue
+++ src/components/todoItem/ItemCounter.vue
@@ -1,48 +1,21 @@
 <template>
-  <div>
-    <v-chip-group>
-      <v-chip class="text-white" label color="blue">
-        {{ `Total Tasks ${inProgress + completed}` }}
-      </v-chip>
-      <v-chip class="text-white" label color="green">
-        {{ `Tasks Completed ${completed}` }}
-      </v-chip>
-      <v-chip class="text-white" label color="orange">
-        {{ `Tasks In Progress ${inProgress}` }}
-      </v-chip>
-    </v-chip-group>
-  </div>
+  <v-chip-group>
+    <v-chip class="text-white" color="blue">
+      {{ `Total Tasks ${inProgress + completed}` }}
+    </v-chip>
+    <v-chip class="text-white" color="green">
+      {{ `Tasks Completed ${completed}` }}
+    </v-chip>
+    <v-chip class="text-white" color="orange">
+      {{ `Tasks In Progress ${inProgress}` }}
+    </v-chip>
+  </v-chip-group>
 </template>
 
 <script>
 export default {
   name: 'ItemCounter',
-  props: ['tasks'],
-  data() {
-    return {
-      completed: 0,
-      inProgress: 0,
-    };
-  },
-  watch: {
-    tasks: {
-      handler: function(newVal) {
-        let tempCompleted = 0;
-        let tempInProgress = 0;
-        newVal.forEach((task) => {
-          if (task.completed) {
-            tempCompleted = tempCompleted + 1;
-          } else {
-            tempInProgress = tempInProgress + 1;
-          }
-
-          this.completed = tempCompleted;
-          this.inProgress = tempInProgress;
-        });
-      },
-      deep: true,
-    },
-  },
+  props: ['inProgress', 'completed'],
 };
 </script>
 
