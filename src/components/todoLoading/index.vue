f9a83ce87684220253c8b8f5081e33fb7a9e21cd
Kegen Guyll <guyllkegen@gmail.com>
2021-03-25 15:44:03 -0500

    created loading component

---
+++ src/components/todoLoading/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="set-height">
+    <v-progress-linear
+      v-if="isLoading"
+      indeterminate
+      color="blue"
+    ></v-progress-linear>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadingBar',
+  props: ['isLoading'],
+};
+</script>
+
+<style scoped>
+.set-height {
+  min-height: 4px;
+}
+</style>
