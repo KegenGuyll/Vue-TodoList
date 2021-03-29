1bf600f0f16ec0f6fe8de3ad66c8a54fe658aa11
Kegen Guyll <guyllkegen@gmail.com>
2021-03-29 09:35:32 -0500

    created errorAlert component

---
+++ src/components/alerts/errorAlert.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-alert v-if="error.message" dense text dismissible type="error">
+    {{ error.message }}
+  </v-alert>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+  name: 'Error Alert',
+  computed: {
+    ...mapGetters({
+      error: 'getError',
+    }),
+  },
+};
+</script>
