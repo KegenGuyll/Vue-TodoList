106eacfd567a0f8b052db220f9bd5614d7ed95fb
Kegen Guyll <guyllkegen@gmail.com>
2021-04-14 00:30:46 -0500

    added mapGetters

--- src/components/todoItem/ItemCounter.vue
+++ src/components/todoItem/ItemCounter.vue
@@ -1,28 +1,35 @@
 <template>
   <v-chip-group show-arrows>
     <v-chip v-on:click="$emit('displayAll')" text-color="white" color="blue">
-      {{ `Total Tasks ${totalCount}` }}
+      {{ `Total Tasks ${allTasksCount}` }}
     </v-chip>
     <v-chip
       v-on:click="$emit('displayCompleted')"
       text-color="white"
       color="green"
     >
-      {{ `Tasks Completed ${completed}` }}
+      {{ `Tasks Completed ${completedTasksCount}` }}
     </v-chip>
     <v-chip
       v-on:click="$emit('displayInProgress')"
       text-color="white"
       color="orange"
     >
-      {{ `Tasks In Progress ${inProgress}` }}
+      {{ `Tasks In Progress ${inProgressTasksCount}` }}
     </v-chip>
   </v-chip-group>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
   name: 'ItemCounter',
-  props: ['inProgress', 'completed', 'totalCount'],
+  computed: {
+    ...mapGetters([
+      'completedTasksCount',
+      'inProgressTasksCount',
+      'allTasksCount',
+    ]),
+  },
 };
 </script>
