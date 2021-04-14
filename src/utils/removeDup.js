eaa1340f9b0ea845acbd0fceaf94eab36b943f73
Kegen Guyll <guyllkegen@gmail.com>
2021-04-14 00:31:59 -0500

    created removeDuplicates

---
+++ src/utils/removeDup.js
@@ -0,0 +1,10 @@
+const removeDuplicates = (array) => {
+  const jsonObject = array.map(JSON.stringify);
+
+  const uniqueSet = new Set(jsonObject);
+  const uniqueArray = Array.from(uniqueSet).map(JSON.parse);
+
+  return uniqueArray;
+};
+
+export default removeDuplicates;
